43fa78e082c62fa82ace05397dffa9ea560b1296
rutorina1 <rutorina1@gmail.com>
2026-06-22 23:13:57 -0600

    crawl page recursive working

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // process
 import { crawlPage } from './crawl.js';
 
-function main() {
+async function main() {
     if (process.argv.length < 3) {
         console.log('no website provided');
         process.exit(1);
@@ -14,7 +14,11 @@ function main() {
     const baseURL = process.argv[2];
 
     console.log(`starting crawl of ${baseURL}...`);
-    crawlPage(baseURL);
+    const pages = await crawlPage(baseURL, baseURL, {});
+
+    for (const [page, count] of Object.entries(pages)) {
+        console.log(`${page} was found ${count} times`);
+    }
 }
 
 main();
